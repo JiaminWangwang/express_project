3cdc8c7ad829ee4b82a3688118303311da69ad2a
wangjm <18246390463@163.com>
2022-02-19 15:23:56 +0800

    完善本地登录流程

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
+// 模拟数据库
+const users = [
+  {username: '张三', password: '123'},
+  {username: '李四', password: '234'},
+]
+
 router.post('/login', function(req, res, next) {
   console.log('成功进入登录接口后端');
 
@@ -13,11 +20,22 @@ router.post('/login', function(req, res, next) {
   console.log(req.body)
   // todo:登录判断
 
-  // 将后端处理结果响应（发送）给前端
-  res.send({
-    message: '登录成功',
-    status: 1
+  const result = users.some((item, index) => {
+    return item.username === req.body.username && item.password === req.body.password
   });
+
+  // 将后端处理结果响应（发送）给前端
+  if (result) {
+    res.send({
+      message: '登录成功',
+      status: 1
+    });
+  } else {
+    res.send({
+      message: '登录失败',
+      status: 0
+    });
+  }
 });
 
 router.post('/register', function(req, res, next) {
